3e8753c3bb22baa8177c590ca1876716505a10f1
Adam Hart <adamhayhart@gmail.com>
2021-09-28 18:48:41 -0700

    updating Heroku PostgreSQL db

--- express-back-end/knexfile.js
+++ express-back-end/knexfile.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-
   development: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      host     : "ec2-52-45-179-101.compute-1.amazonaws.com:5432",
-      user     : "vxrdjlrraaxbts",
-      password : "a6a15c67c1ab9e21d885ea60be04c1cecd8dce964e12cb3c2cafeeecd35f7949",
-      database : "d7e2i61onkfefs",
-      port     : "5432",
-      ssl      : process.env.DB_SSL,
+      host: "ec2-3-218-71-191.compute-1.amazonaws.com",
+      user: "vbcvsfkqqudldy",
+      password:
+        "b827b5260545d30e1bf2522f11f7e2ae86410c31a0898de7c6ae22e598207c4e",
+      database: "d53rhfchq6e6nd",
+      port: "5432",
+      ssl: process.env.DB_SSL,
+      uri: "postgres://vbcvsfkqqudldy:b827b5260545d30e1bf2522f11f7e2ae86410c31a0898de7c6ae22e598207c4e@ec2-3-218-71-191.compute-1.amazonaws.com:5432/d53rhfchq6e6nd",
+      cli: "heroku pg:psql postgresql-adjacent-17920 --app inkedin-db",
       // host     : process.env.DB_HOST,
       // user     : process.env.DB_USER,
       // password : process.env.DB_PASS,
@@ -19,26 +21,23 @@ module.exports = {
       // ssl      : process.env.DB_SSL,
     },
     migrations: {
-      directory: './db/migrations',
-      tableName: 'migrations',
+      directory: "./db/migrations",
+      tableName: "migrations",
     },
     seeds: {
-      directory: './db/seeds',
+      directory: "./db/seeds",
     },
   },
 
   production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    client: "postgresql",
+    connection: process.env.DATABASE_URL + "?ssl=true",
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: 'migrations',
+      tableName: "migrations",
     },
   },
-
 };
-
-
